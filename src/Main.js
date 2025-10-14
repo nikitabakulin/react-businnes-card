cddf31bcc11bfa1fc0ce496ec67389c8e7169618
nikitabakulin <bakulinnikita77@gmail.com>
2025-10-14 11:56:50 +0300

    Add FooterNavigation

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ModalWindow from './ModalWindow';
+import Theme from './Theme';
 
 function Main() {
+  const { theme, setTheme } = Theme();
+  const [ isDarkTheme, setDarkTheme ] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'dark';
+  });
+
+  const toggleTheme = () => {
+    if (isDarkTheme) {
+      lightTheme();
+    } else {
+      darkTheme();
+    }
+
+    setDarkTheme(!isDarkTheme);
+  };
+
+  const lightTheme = () => {
+    setTheme('light');
+  };
+
+  const darkTheme = () => {
+    setTheme('dark');
+  };
+
   const [showModal, setShowModal] = useState(false);
   const handleOpenModal = () => {
     setShowModal(true);
@@ -10,36 +35,58 @@ function Main() {
     setShowModal(false);
   };
 
+  const [scroll, setScroll] = useState(0);
+
+  const scrollUp = () => {
+    setScroll(window.scrollY);
+  };
+
+  const upButton = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollUp);
+  });
+  
+  const toBlock = (height) => {
+    window.scrollTo({ top: height, left: 0, behavior: "smooth" });
+  }
+
   return (
     <div>
       <header>
-        <div className="menu">
-          <a>О себе</a>
-          <a>Услуги</a>
-          <a>Портфолио</a>
-          <a>Отзывы</a>
-          <a>Гарантии</a>
+        <div className='navigation'>
+          <div className="menu">
+            <a onClick={ upButton }>Главная</a>
+            <a onClick={ (e) => toBlock(e.target.getAttribute('height')) } height="700"> Навыки </a>
+            <a onClick={ (e) => toBlock(e.target.getAttribute('height')) } height="1230"> GitHub проекты </a>
+            <a onClick={ (e) => toBlock(e.target.getAttribute('height')) } height="1920"> О себе </a>
+          </div>
+    
+          <div className='header-buttons'>
+            <button onClick={ handleOpenModal } className="btn">Связаться</button>
+
+            <a href="https://t.me/sid10Juve" target="_blank" className={ theme == 'light' ? "icon telegram light" : "icon telegram" }/>
+            <a href="https://vk.com/nikita_bakulin" target="_blank" className={ theme == 'light' ? "icon instagram light" : "icon instagram" }/>
+
+            <div className="switch" onClick={ toggleTheme }> 
+              <div className={theme === 'light' ? "theme light" : "theme dark"}
+                style={{ transform: isDarkTheme ? 'translateX(38px)' : 'translate(0)'}}
+              ></div>
+            </div>
+          </div>
         </div>
-
-        <button onClick={handleOpenModal} className="btn">Связаться</button>
-
-        <ModalWindow show={showModal} onClose={handleCloseModal}>
-          <h2 style={{color: "#4824ff", fontSize: "40px"}}>Контакты</h2>
-          <p style={{fontSize: "22px"}}>Вы можете связаться со мной в Телеграм <br/> или ВКонтакте</p>
-        </ModalWindow>
-
-        <a href="https://t.me/sid10Juve" target="_blank" className="icon telegram"/>
-        <a href="https://vk.com/nikita_bakulin" target="_blank" className="icon instagram"/>
-
-        <div className="switch"> 
-          <div className="theme light"></div>
-        </div>
-
       </header>
+
+      <ModalWindow show={ showModal } onClose={ handleCloseModal }>
+        <h2 style={{ color: "#4824ff", fontSize: "40px" }}>Контакты</h2>
+        <p style={{ fontSize: "22px" }}>Вы можете связаться со мной в Телеграм <br/> или ВКонтакте</p>
+      </ModalWindow>
 
       <div className='welcome-block'>
         <div className='first-block'>
-          <h1> Программист-разработчик <span className='title'> nikita_bakulin</span> </h1>
+          <h1> Программист-разработчик <br/> <span className='title'> nikitabakulin</span> </h1>
           
           <h2 style={{ marginTop: "5%" }}>  
             <span style={{ color: "#4824ff" }}> Backend </span>
@@ -121,7 +168,15 @@ function Main() {
         <p style={{ fontSize: "27px" }} className='tag'> <p className='tag-icon'/> Изучаю английский для профессионального роста и работы с международной документацией </p>
         <p style={{ fontSize: "27px" }} className='tag'> <p className='tag-icon'/> Быстро осваиваю новые технологии и стеки </p>
         <p style={{ fontSize: "27px" }} className='tag'> <p className='tag-icon'/> Есть водительское удостоверение и автомобиль </p>
+        <p style={{ fontSize: "27px" }} className='tag'> <p className='tag-icon'/> Не  пью, не курю </p>
       </div>
+
+      <div className='footer'>@nikitabakulin</div>
+
+      <button 
+        className={ scroll < 720 ? "" : "btn-up" }
+        onClick={ upButton }
+      ></button>
     </div>
   );
 } 
